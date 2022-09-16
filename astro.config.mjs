7c24d296baf794b6a2817711cb15189dcac9d8c4
Nate Emerson <emerson.nate@gmail.com>
2022-09-15 21:38:00 -0700

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import tailwind from "@astrojs/tailwind";
+
+const tailwindOptions = {
+  config: {
+    applyBaseStyles: false
+  }
+}
+
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+  integrations: [preact(), tailwind()]
 });
